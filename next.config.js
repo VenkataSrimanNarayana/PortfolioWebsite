7dd9ab23a505506166fd28549cd1491c955590f1
Venkata Sriman <venkata.sriman@iitgn.ac.in>
2023-10-23 03:32:32 +0530

    test

--- next.config.js
+++ next.config.js
@@ -9,16 +9,6 @@ const nextConfig = {
             transform: "@mui/icons-material/{{member}}",
         },
     },
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "source.unsplash.com",
-                port: "",
-                pathname: "/random",
-            },
-        ],
-    },
 };
 
 module.exports = nextConfig;
