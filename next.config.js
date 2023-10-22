0b87dcf1090d8196a1a4d503096e6456ea823ffd
Venkata Sriman <venkata.sriman@iitgn.ac.in>
2023-10-23 03:45:37 +0530

    modify next js config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const prod = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+    basePath: prod ? "/PortfolioWebsite" : "",
     output: "export",
     distDir: "build",
     reactStrictMode: true,
